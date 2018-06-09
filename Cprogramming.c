c20114145ace2c27e4939d934ffd77c20f77a20b
Timnoggin <39819883+Timnoggin@users.noreply.github.com>
2018-06-09 12:13:14 +1000

    Add files via upload

---
+++ Cprogramming.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+
+int main(){
+    int a;
+	printf("hello world\nand\nhello mum and dad!\n");
+	
+	a = 12;
+	printf("%d\n", a); 
+	return 0;
+}
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
